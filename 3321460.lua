6826cfde2dec4c31d77a957a5998cb7e1b03e6fb
xlfengye <2476083228@qq.com>
2026-05-23 00:12:25 +0800

    Add files via upload

---
+++ 3321460.lua
@@ -0,0 +1,18 @@
+-- ============================================================
+-- 官方网站: https://steamforge.hyper99.top/
+-- 联系 QQ: 2476083228
+-- 入库时间: 2026-05-23 00:08:28
+-- 游戏 AppID: 3321460
+-- ============================================================
+
+-- Generated for AppID 3321460 (branch: public)
+-- Mode: Auto-update (no setManifestid)
+addappid(3321460)
+addappid(3321461, 0, "e2e570c84eb8b4cfe9c20268cd30b84f8c1c7c076691c92ae8c5ba7b510b8a95")
+addappid(3321462, 0, "6eb3bfe7d16b9eb6441dba4f7d18b341fe83ab2f1e9385d042ce622f38a600cb")
+setManifestid(3321461,"8432047928063861989")
+
+-- DLCs
+addappid(4024620)
+addappid(4024630)
+addappid(4193060)
